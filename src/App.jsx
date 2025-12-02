a40e0070f1bf472f22898208f493678cd6e75135
Daniel Ohinoyi <bizwithdan@gmail.com>
2025-12-02 14:18:14 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Vite + React</h1>
+        <div className="card">Let's Build A ReactJS Dashboard Template</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
